5eaba6412b33e1e46edccf8dfe4394b89dc6723b
Brian Faust <hello@brianfaust.me>
2017-12-08 08:19:54 +0200

    initial commit

---
+++ index.js
@@ -0,0 +1,18 @@
+var bip39 = require("bip39");
+var ark = require('arkjs');
+var argv = require('minimist')(process.argv.slice(2));
+
+if (argv['n']) {
+    ark.crypto.setNetworkVersion({
+        mainnet: 17,
+        testnet: 52,
+        devnet: 30
+    }[argv['n']]);
+}
+
+for (i = 0; i < argv['a']; i++) {
+    var passphrase = bip39.generateMnemonic();
+    var address = ark.crypto.getAddress(ark.crypto.getKeys(passphrase).publicKey);
+
+    console.log("Address: " + address + " | Passphrase: " + passphrase);
+}
